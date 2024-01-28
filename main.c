1b91e24da5afb2fa2d361cc54b30514674cc7596
Varun Gole <varungole1999@gmail.com>
2024-01-29 02:35:49 +0530

    commit

---
+++ main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+struct CheckIn
+{
+  int bags;
+  int weight_check;
+};
+
+int board(int bags)
+{
+return 0;
+}
+int main()
+{
+ printf("Welcome to Mumbai Airport! \n");
+ printf("Tell us the Flight you are catching today \n");
+
+ printf("1. Cathy Pacific 2. Etihad 3.Air France \n");
+ int option;
+ scanf("%d" , &option);
+ 
+ if(option == 1)
+ {
+    printf("Thanks for Choosing Cathy Pacific");
+
+    printf("Please proceed to Boarding");
+
+    
+ }
+ return 0;    
+}
